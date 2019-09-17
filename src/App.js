38b1fcd24562112e9823bd0fe0b7f34d7b093bba
danfyfe <fyfedaniel@gmail.com>
2019-09-17 15:15:47 -0400

    coconut click drop animation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, {  } from 'react';
 import './App.css';
 
 function App() {
+
+
+  const dropCoconut = () => {
+    const fullCoconut = document.getElementById('full-coconut')
+    fullCoconut.className = 'falling'
+    setTimeout(breakCoconut,500)
+  }
+
+  const breakCoconut = () => {
+    const fullCoconut = document.getElementById('full-coconut')
+    const treeContainer = document.querySelector('.tree-container')
+    const leftHalfCoconut = document.createElement('img')
+    leftHalfCoconut.src = 'https://i.imgur.com/r7eQv0P.png'
+    leftHalfCoconut.className = 'half-coconut left-half'
+    const rightHalfCoconut = document.createElement('img')
+    rightHalfCoconut.src = 'https://i.imgur.com/r7eQv0P.png'
+    rightHalfCoconut.className = 'half-coconut right-half'
+
+    treeContainer.appendChild(leftHalfCoconut)
+    treeContainer.appendChild(rightHalfCoconut)
+
+
+
+
+
+    fullCoconut.remove()
+  }
+
   return (
     <div className='main-wrapper d-flex flex-column'>
       <div className='sky'>
@@ -19,8 +47,7 @@ function App() {
       <div className='sand'>
         <div className='tree-container'>
           <img id='coconut-tree'src='https://i.imgur.com/LQlH63o.png' alt='cartoon coconut tree'/>
-          <img id='full-coconut' src='https://i.imgur.com/Xv80fwu.png' alt='full coconut'/>
-          <img id='half-coconut' src='https://i.imgur.com/r7eQv0P.png' alt='half coconut'/>
+          <img id='full-coconut' src='https://i.imgur.com/Xv80fwu.png' alt='full coconut' onClick={dropCoconut}/>
         </div>
       </div>
 
@@ -31,3 +58,5 @@ function App() {
 export default App;
 
 //   <img id='palm-tree' src='https://i.imgur.com/LQlH63o.png' alt='palm tree'/>
+
+// <img id='half-coconut' src='https://i.imgur.com/r7eQv0P.png' alt='half coconut'/>
