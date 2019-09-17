70be90496e380b428a4e2f7c10a8a82cf4c65b68
danfyfe <fyfedaniel@gmail.com>
2019-09-17 16:12:08 -0400

    coconut animation

--- src/App.js
+++ src/App.js
@@ -5,29 +5,46 @@ function App() {
 
 
   const dropCoconut = () => {
-    const fullCoconut = document.getElementById('full-coconut')
+    const fullCoconut = document.querySelector('.full-coconut')
     fullCoconut.className = 'falling'
     setTimeout(breakCoconut,500)
   }
 
+  const resetShake = () => {
+    const fullCoconut = document.querySelector('.full-coconut')
+    setTimeout( () => {
+      fullCoconut.animationName = 'shake';
+    }, 1000)
+  }
+
   const breakCoconut = () => {
-    const fullCoconut = document.getElementById('full-coconut')
+    const fullCoconut = document.querySelector('.falling')
+
     const treeContainer = document.querySelector('.tree-container')
+
     const leftHalfCoconut = document.createElement('img')
     leftHalfCoconut.src = 'https://i.imgur.com/r7eQv0P.png'
     leftHalfCoconut.className = 'half-coconut left-half'
+
     const rightHalfCoconut = document.createElement('img')
     rightHalfCoconut.src = 'https://i.imgur.com/r7eQv0P.png'
     rightHalfCoconut.className = 'half-coconut right-half'
 
+    const titleText = document.createElement('span')
+    titleText.innerText = "Dan's Coconuts"
+    titleText.className = 'title-text'
+
+
     treeContainer.appendChild(leftHalfCoconut)
     treeContainer.appendChild(rightHalfCoconut)
 
+    treeContainer.appendChild(titleText)
 
 
 
 
     fullCoconut.remove()
+
   }
 
   return (
@@ -46,12 +63,13 @@ function App() {
 
       <div className='sand'>
         <div className='tree-container'>
-          <img id='coconut-tree'src='https://i.imgur.com/LQlH63o.png' alt='cartoon coconut tree'/>
-          <img id='full-coconut' src='https://i.imgur.com/Xv80fwu.png' alt='full coconut' onClick={dropCoconut}/>
+          <img id='coconut-tree' src='https://i.imgur.com/LQlH63o.png' alt='cartoon coconut tree'/>
+          <img id='full-coconut' className='full-coconut' src='https://i.imgur.com/Xv80fwu.png' alt='full coconut' onClick={dropCoconut}/>
         </div>
       </div>
-
+      
     </div>
+
   );
 }
 
