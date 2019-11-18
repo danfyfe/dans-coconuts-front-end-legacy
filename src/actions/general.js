fb5f55b513bf0ae6589cb759c14740179435f274
danfyfe <fyfedaniel@gmail.com>
2019-11-18 10:10:47 -0500

    small cleanup

--- src/actions/general.js
+++ src/actions/general.js
@@ -1,11 +1,10 @@
 export const scrollToElement = selector => {
     const element = document.querySelector(`${selector}`);
     const pos = element.getBoundingClientRect()
-    console.log(pos.top)
     if( pos.top > 200) {
         window.scrollTo({
           top: pos.top,
           behavior: 'smooth'
         });
     }
-  }
+}
