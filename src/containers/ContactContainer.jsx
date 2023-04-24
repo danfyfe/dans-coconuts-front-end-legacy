e889ed6dcf1a7f4d7756697931b39e142baaea4b
Dan Fyfe <fyfedaniel@gmail.com>
2023-04-24 15:50:07 -0400

    update resume link

--- src/containers/ContactContainer.jsx
+++ src/containers/ContactContainer.jsx
@@ -4,7 +4,7 @@ const ContactContainer = props => {
     return(
         <div className='contact-container'>
             
-            <a className='contact-link' href='https://drive.google.com/file/d/1k-2jrLLPZuerPnZN7E4ZsP27RrwlISLo/view?usp=share_link' target='_blank' rel='noopener noreferrer'>resume</a>
+            <a className='contact-link' href='https://drive.google.com/file/d/1SPManG0ThM4YVR2W-er_DmzfjzTIMvSN/view?usp=share_link' target='_blank' rel='noopener noreferrer'>resume</a>
 
             <a className='contact-link' href='mailto: fyfedaniel@gmail.com'
             target='_blank' rel='noopener noreferrer'>fyfedaniel@gmail.com
